a356d7c04e02ab9239302c924aed7c81ffc1fc44
Ruan Vitor <92278015+Rvitorr@users.noreply.github.com>
2021-10-24 03:13:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'Rvitorr',
+  youtube: 'channel/UCrabAhvDQI45yjUKcm6yPQw',
+  linkedin: 'in/ruan-cordeiro-5873a4216',
+  instagram: 'ru_vitor',
+  twitter: 'ru_art2'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.href = data.login
+    })
+}
+
+getGithubProfileInfos()
